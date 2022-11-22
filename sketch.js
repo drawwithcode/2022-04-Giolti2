562d249f12295d2340c7e3b7587edb1fc043beb4
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-11-22 21:31:52 +0100

    Chief I'm tired
    -added mobile support
    -added board frame
    -added eraser silder
    -rotating device breaks everything, will probably get back to this
    -my worst summary so far, I just want to rest

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 let hexRadius;
 
+let shakelock = 0;
+let textonce = true;
+
+let slider = document.getElementById("slider");
+
 let grid = [];
 
 class Hex{
@@ -38,11 +43,31 @@ class Hex{
 		pop();
 	}
 
-	mouseOn() {
-		return dist(mouseX, mouseY, this.centerX, this.centerY) < hexRadius * sin(60)
+	sliderOn() {
+		let sliderX = map(slider.value, 1, 100, width * 0.05, width * 0.95);
+		
+		return (Math.abs(this.centerX - sliderX) < 0.1 * width)
 	}
 
-	adjacentSetup() {
+	mouseOn(x, y) {
+		return dist(x, y, this.centerX, this.centerY) < hexRadius * sin(60)
+	}
+
+	stroke() {
+		this.fill = 80 + random(20);
+		this.render = true;
+		this.timelock = 20;
+
+		this.adjacent.forEach(function (hex) {
+			if (hex.timelock == 0) {
+				hex.fill = ((80 + random(20))*2 + hex.fill) / 3;
+				hex.render = true;
+				hex.timelock = 20;
+			}
+		})
+	}
+
+	adjacentSetup(grid) {
 		try {
 			this.adjacent.push(grid[this.x - 1 + this.y % 2][this.y - 1])
 		}catch{}
@@ -86,9 +111,11 @@ function preload() {
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
+	document.getElementById("box").style.height = (windowHeight - 0.15 * windowWidth) + "px"
+
 	frameRate(120)
 	
-	hexRadius = 0.005 * height;
+	hexRadius = 0.005 * max(width, height);
 
 	angleMode(DEGREES)
 
@@ -101,38 +128,111 @@ function setup() {
 
 	grid.forEach(function (col) {
 		col.forEach(function (hex) {
-			hex.adjacentSetup();
+			hex.adjacentSetup(grid);
 		})
 	})
 }
 
 function draw() {
-	// put drawing code here
 	grid.forEach(function (col) {
 		col.forEach(function (hex) {
-			if (mouseIsPressed === true) {
-				if (hex.mouseOn()) {
-					hex.fill = 80 + random(20);
-					hex.render = true;
 
-					hex.adjacent.forEach(function (hex) {
-						if (hex.timelock == 0) {
-							hex.fill = ((80 + random(20))*2 + hex.fill) / 3;
-							hex.render = true;
-							hex.timelock = 20;
-						}
-					})
+			if (hex.sliderOn()) {
+				let sliderX = map(slider.value, 0, 100, width * 0.05, width * 0.95, true);
+				let newFill = map(Math.abs(hex.centerX - sliderX), width * 0.03, width * 0.1, 0, 100, true)
+
+				if (newFill < hex.fill) {
+					hex.fill = newFill;
+					hex.render = true;
+					hex.timelock = 20;
 				}
 			}
+
+			if (mouseIsPressed === true) {
+				if (hex.mouseOn(mouseX, mouseY)) {
+					hex.stroke();
+				}
+			}
+
+			if (touches.length > 0) {
+				touches.forEach(function (touch) {
+					if (hex.mouseOn(touch.x, touch.y)) {
+						hex.stroke();
+					}
+				})
+			}
+
 			if (hex.render) {
 				hex.draw();
 				hex.render = false;
 			}
+
 			hex.update();
 		})
 	})
 
-	if (frameRate() < 50) {
-		console.log(frameRate() + " t: " + millis()/1000)
+	if (shakelock > 0) {
+		shakelock--;
+	}
+
+	if (textonce) {
+		push()
+
+		translate(width / 2, height / 2)
+		
+		textSize(16);
+		textAlign(CENTER, CENTER);
+
+		text('Draw with your fingers and erase with the slider', 0, 0,);
+		text('Shake the device to scramble things a bit', 0, 20);
+		
+		pop()
+
+		textonce = false;
+	}
+}
+
+function deviceShaken() {
+	if (shakelock == 0) {
+		let newGrid = [];
+
+		grid.forEach(function (col) {
+			newGrid.push([])
+
+			col.forEach(function (hex) {
+				newGrid[hex.x].push(new Hex(hex.x, hex.y))
+			})
+		})
+
+		newGrid.forEach(function (col) {
+			col.forEach(function (hex) {
+				hex.adjacentSetup(newGrid);
+			})
+		})
+
+		grid.forEach(function (col) {
+			col.forEach(function (hex) {
+				let partFill = hex.fill / 7;
+				if(hex.x == 30 && hex.y == 40)console.log(hex)
+				
+				newGrid[hex.x][hex.y].fill += random(0.5, 1.5) * partFill;
+
+				hex.adjacent.forEach(function (hex) {
+					newGrid[hex.x][hex.y].fill += random(0.5, 1.5) * partFill;
+				})
+			})
+		})
+
+		grid.forEach(function (col) {
+			col.forEach(function (hex) {
+				if (newGrid[hex.x][hex.y].fill != hex.fill) {
+					newGrid[hex.x][hex.y].render = true;
+				}
+			})
+		})
+		
+		grid = newGrid.slice();
+
+		shakelock = 80;
 	}
 }
